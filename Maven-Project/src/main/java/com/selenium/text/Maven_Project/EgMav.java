c9b796dc25cf17ced570f25ea5f763229dd69b4d
brindhadevim <brindhadevim@MDTR21.mlad.in>
2019-07-04 16:55:35 +0530

    Hai hai

--- Maven-Project/src/main/java/com/selenium/text/Maven_Project/EgMav.java
+++ Maven-Project/src/main/java/com/selenium/text/Maven_Project/EgMav.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class EgMav {
 
 	public static void main(String[] args) {
+		//brindhajk 
+		//Birujk
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\brindhadevim\\Desktop\\selenium\\chromedriver.exe");
 		 WebDriver driver=new ChromeDriver();
 		 driver.get("https://www.irctc.co.in/eticketing/pageUnderConstruction.jsf");
+		 
 	}
 
 }
